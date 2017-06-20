b978261de7687a6d4315dbb7bdda9cf90eab9ff0
Jesse Tomchak <jtomchak@gmail.com>
2017-06-19 22:24:25 -0700

    day 2 oh boy!!

---
+++ Day2/HandsOn.js
@@ -0,0 +1,7 @@
+//TODO
+// In this project, we will be building what is called Bing Bong.This application will loop through a list of
+//   0 - 100. For each number,
+//   if it is divisible by 3, you will print 'Bing'
+// to the console.If it is divisible by 5, you will print 'Bong'
+// to the console.If the number is divisible by 3 and 5, you will print both 'Bing'
+// and 'Bong'.
---
+++ Day2/StringAndTemplateLiterals.js
@@ -0,0 +1,16 @@
+function greatPants() {
+  return "great pants";
+}
+//TODO
+//Use Template literals to console out the phrase
+// => "Dockers, man those are great pants"
+
+
+
+
+//TODO
+//=> "HARRY POTTER is the greatest wizard ever!!!!!"
+// notice we're shouting his name, all uppercase please
+var user = {
+  name: 'Harry Potter'
+};
