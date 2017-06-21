c11cf3f7b48cb66ee3da5a3ea9f693d89335b054
Jesse Tomchak <jtomchak@gmail.com>
2017-06-21 09:06:29 -0700

    more functions

---
+++ Day3/ClassesInheritance.js
@@ -0,0 +1,12 @@
+//TODO Fancy new ES 2015 Class
+//remake our Meat and Produce class with calculatax method, name, and price
+//as a fancy new class and not a function constructor, below is a template to help guide you, Vehicle is only an example of basic structor.
+class Vehicle {
+  constructor() {}
+  topSpeed() {
+    //class method code here
+    return "How fast does it go?";
+  }
+}
+let momMinivan = new Vehicle();
+console.log(momMinivan.topSpeed());
