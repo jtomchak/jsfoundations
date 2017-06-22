8e57c031bda9037572e87efd98055bd3a6cd30de
Jesse Tomchak <jtomchak@gmail.com>
2017-06-22 10:18:25 -0700

    git and frameworks

---
+++ Day4/GitStuff.js
@@ -0,0 +1,16 @@
+//User/Documents/jsfoundations/
+//make sure ATOM is close
+
+cd.. //directory
+
+pwd //where am i
+
+git commit - am "added new stuff"
+//git add . is the same as the '-a' on the commit
+
+git status //clean tree
+
+git pull //new shinny stuff
+
+git checkout day4 //switch to branch 'day4' from where ever
+//DON'T STEAL BOOKS
