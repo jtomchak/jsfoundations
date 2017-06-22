8429e207a71f1efb9e4032010064b7bd3cc6531e
Jesse Tomchak <jtomchak@gmail.com>
2017-06-22 06:31:11 -0700

    another this example

--- Day3/ArrayPrototype.js
+++ Day3/ArrayPrototype.js
@@ -1,6 +1,7 @@
 // is pineapple in the array?
 
 var fruits = ["apple", "banana", "orange"];
+var otherStuff = [2, 4, 5, 6, 4, 3];
 Array.prototype.contains = function() {
   var i = this.length;
   while (i--) {
